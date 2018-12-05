876b712ace9ae67a6f3afec6a70a5cd67f7b8e0c
mtamahori <mtamahori@gmail.com>
2018-12-05 11:40:30 -0600

    initial associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,37 @@
 const User = require('./user')
+const Booker = require('./booker')
+const Deejay = require('./deejay')
+const Gig = require('./gig')
+const Message = require('./message')
+const Review = require('./review')
+
 
 // Associations
+
+Deejay.hasMany(Gig)
+Deejay.hasMany(Message)
+Deejay.hasMany(Review)
+
+Gig.belongsTo(Deejay)
+Message.belongsTo(Deejay)
+Review.belongsTo(Deejay)
+
+Booker.hasMany(Gig)
+Booker.hasMany(Message)
+Booker.hasMany(Review)
+
+Gig.belongsTo(Booker)
+Message.belongsTo(Booker)
+Review.belongsTo(Booker)
 
 
 // Central export for model definitions
 
 module.exports = {
-  User
+  User,
+  Booker,
+  Deejay,
+  Gig,
+  Message,
+  Review
 }
