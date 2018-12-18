b7796e727c4d95f08c9ac3d177ab1b6185442e20
mtamahori <mtamahori@gmail.com>
2018-12-18 08:45:39 -0600

    seed promise each

--- script/seed.js
+++ script/seed.js
@@ -1,63 +1,68 @@
 'use strict'
 
 const db = require('../server/db')
+const Promise = require('bluebird');
 const { User, Booker, Deejay, Gig, Message, Review } = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'max@user.com', password: '123'}),
-    User.create({email: 'meri@user.com', password: '123'}),
-    User.create({email: 'miles@user.com', password: '123'}),
-    User.create({email: 'maeve@user.com', password: '123'}),
-    User.create({email: 'jason@user.com', password: '123'}),
-    User.create({email: 'jake@user.com', password: '123'}),
-    User.create({email: 'ray@user.com', password: '123'}),
-    User.create({email: 'tom@user.com', password: '123'}),
-    User.create({email: 'paddy@user.com', password: '123'}),
-    User.create({email: 'pam@user.com', password: '123'}),
-  ])
+  const users = await Promise.each([
+    {email: 'max@user.com', password: '123'},
+    {email: 'meri@user.com', password: '123'},
+    {email: 'miles@user.com', password: '123'},
+    {email: 'maeve@user.com', password: '123'},
+    {email: 'jason@user.com', password: '123'},
+    {email: 'jake@user.com', password: '123'},
+    {email: 'ray@user.com', password: '123'},
+    {email: 'tom@user.com', password: '123'},
+    {email: 'paddy@user.com', password: '123'},
+    {email: 'pam@user.com', password: '123'},
+  ], (user) => {
+    return User.create(user);
+  })
 
-  const bookers = await Promise.all([
-    Booker.create({name: 'Max', email: 'max@booker.com', phone: '3108693703', userId: 1}),
-    Booker.create({name: 'Meri', email: 'meri@booker.com', phone: '7818012471', userId: 2}),
-    Booker.create({name: 'Miles', email: 'miles@booker.com', phone: '4043846952', userId: 3}),
-    Booker.create({name: 'Maeve', email: 'maeve@booker.com', phone: '2072660335', userId: 4}),
-  ])
+  const bookers = await Promise.each([
+    {name: 'Max', email: 'max@booker.com', phone: '3108693703', userId: 1},
+    {name: 'Meri', email: 'meri@booker.com', phone: '7818012471', userId: 2},
+    {name: 'Miles', email: 'miles@booker.com', phone: '4043846952', userId: 3},
+    {name: 'Maeve', email: 'maeve@booker.com', phone: '2072660335', userId: 4},
+    {name: 'Jason', email: 'jason@booker.com', phone: '0123456789', userId: 5}
+  ], (booker) => {
+    return Booker.create(booker);
+  })
 
-  const deejays = await Promise.all([
-    Deejay.create({name: 'Max', email: 'max@deejay.com', phone: '3108693703', userId: 1}),
-    Deejay.create({name: 'Meri', email: 'meri@deejay.com', phone: '7818012471', userId: 2}),
-    Deejay.create({name: 'Miles', email: 'miles@deejay.com', phone: '4043846952', userId: 3}),
-    Deejay.create({name: 'Maeve', email: 'maeve@deejay.com', phone: '2072660335', userId: 4})
-  ])
+  const deejays = await Promise.each([
+    {name: 'Max', email: 'max@deejay.com', phone: '3108693703', userId: 1},
+    {name: 'Meri', email: 'meri@deejay.com', phone: '7818012471', userId: 2},
+    {name: 'Miles', email: 'miles@deejay.com', phone: '4043846952', userId: 3},
+    {name: 'Maeve', email: 'maeve@deejay.com', phone: '2072660335', userId: 4}
+  ], (deejay) => {
+    return Deejay.create(deejay);
+  })
 
-  const gigNameList = [];
-  const gigDateList = [];
-  const gigTimeList = [];
-  const gigLocList = [];
-  const gigCompList = [];
-  const gigDeejayList = [];
-  const gigBookerList = [];
+  const gigs = await Promise.each([
+    {name: 'Bar Night', date: '12/05/18', time: '22:00-01:00', location: 'UChicago', compensation: '20', deejayId: 1, bookerId: 4},
+    {name: 'Erotic Wobble', date: '12/06/18', time: '21:00-02:00', location: 'Hyde Park', compensation: '30', deejayId: 2, bookerId: 3},
+    {name: 'METER', date: '12/07/18', time: '21:00-01:00', location: 'Meter Room', compensation: '40', deejayId: 3, bookerId: 2},
+    {name: 'Humboldt Arboreal Society', date: '12/08/18', time: '15:00-20:00', location: 'Humboldt Park', compensation: 'fun', deejayId: 4, bookerId: 1},
+    {name: 'Punch House', date: '1/01/19', time: '22:00-2:00', location: 'Punch House', compensation: '150', deejayId: 1, bookerId: 4},
+    {name: 'Smartbar', date: '1/03/19', time: '22:00-4:00', location: 'Smartbar', compensation: '150', deejayId: 1, bookerId: 5},
 
-  const gigs = await Promise.all([
-    Gig.create({name: 'Bar Night', date: '12/05/18', time: '22:00-01:00', location: 'UChicago', compensation: '20', deejayId: 1, bookerId: 4}),
-    Gig.create({name: 'Erotic Wobble', date: '12/06/18', time: '21:00-02:00', location: 'Hyde Park', compensation: '30', deejayId: 2, bookerId: 3}),
-    Gig.create({name: 'METER', date: '12/07/18', time: '21:00-01:00', location: 'Meter Room', compensation: '40', deejayId: 3, bookerId: 2}),
-    Gig.create({name: 'Humboldt Arboreal Society', date: '12/08/18', time: '15:00-20:00', location: 'Humboldt Park', compensation: 'fun', deejayId: 4, bookerId: 1}),
+    {name: 'Max tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 1},
+    {name: 'Meri tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 2},
+    {name: 'Miles tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 3},
+    {name: 'Maeve tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 4},
 
-    Gig.create({name: 'Punch House', date: '1/01/19', time: '22:00-2:00', location: 'Punch House', compensation: '150', bookerId: 4 }),
-    Gig.create({name: 'The Whistler', date: '1/02/19', time: '22:00-2:00', location: 'The Whistler', compensation: '150', bookerId: 4 }),
-    Gig.create({name: 'Smartbar', date: '1/03/19', time: '22:00-4:00', location: 'Smartbar', compensation: '150', bookerId: 3}),
-    Gig.create({name: 'Danny\'s', date: '1/04/19', time: '22:00-2:00', location: 'Danny\'s', compensation: '150', bookerId: 4}),
-
-    Gig.create({name: 'Max tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 1}),
-    Gig.create({name: 'Miles tryna spin', date: 'whenever', time: 'all night', location: '', compensation: '100', deejayId: 3}),
-    Gig.create({name: 'Open Humboldt', date: 'whenever', time: 'all day', location: 'Humboldt Park', compensation: 'fun', bookerId: 1}),
-    Gig.create({name: 'Open Smartbar', date: 'whenever', time: 'all night', location: 'smartbar', compensation: '250', bookerId: 4}),
-  ])
+    {name: 'Open Humboldt', date: 'whenever', time: 'all day', location: 'Humboldt Park', compensation: 'fun', bookerId: 1},
+    {name: 'Open Danny\'s', date: '1/04/19', time: '22:00-2:00', location: 'Danny\'s', compensation: '150', bookerId: 2},
+    {name: 'Open Whistler', date: '1/02/19', time: '22:00-2:00', location: 'The Whistler', compensation: '150', bookerId: 3},
+    {name: 'Open Constellation', date: '1/03/19', time: '22:00-2:00', location: 'Constellation', compensation: '150', bookerId: 4},
+    {name: 'Open Smartbar', date: 'whenever', time: 'all night', location: 'smartbar', compensation: '250', bookerId: 5}
+  ], (gig) => {
+    return Gig.create(gig);
+  })
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${bookers.length} bookers`)
