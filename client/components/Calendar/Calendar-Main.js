88ac50247da60e13da35e306207eb1597f5eb6bf
mtamahori <mtamahori@gmail.com>
2019-01-23 13:55:30 -0600

    calendar header init

--- client/components/Calendar/Calendar-Main.js
+++ client/components/Calendar/Calendar-Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import dateFns from 'date-fns'
+import './calendar.css'
 
 class CalendarMain extends Component {
   constructor(props) {
@@ -10,6 +11,11 @@ class CalendarMain extends Component {
       currentMonth: new Date(),
       selectedDate: new Date
     }
+
+    //bindings
+    // this.onDateClick = this.onDateClick.bind(this);
+    // this.nextMonth = this.nextMonth.bind(this);
+    // this.prevMonth = this.prevMonth.bind(this);
   }
 
 
@@ -24,7 +30,22 @@ class CalendarMain extends Component {
   }
 
   renderHeader() {
-
+    const dateFormat = "MMMM YYYY";
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={this.prevMonth}>chevron_left</div>
+        </div>
+        <div className="col col-center">
+          <span>
+            {dateFns.format(this.state.currentMonth, dateFormat)}
+          </span>
+        </div>
+        <div className="col col-end" onClick={this.nextMonth}>
+          <div className="icon" onClick={this.nextMonth}>chevron_right</div>
+        </div>
+      </div>
+    )
   }
 
   renderDays() {
@@ -40,11 +61,15 @@ class CalendarMain extends Component {
   }
 
   nextMonth = () => {
-
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    })
   }
 
   prevMonth = () => {
-
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    })
   }
 }
 
