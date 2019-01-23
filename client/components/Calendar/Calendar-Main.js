586daa2cf517b1541bf0ec82297cd458e5e02078
mtamahori <mtamahori@gmail.com>
2019-01-23 14:32:31 -0600

    render days, needs styling

--- client/components/Calendar/Calendar-Main.js
+++ client/components/Calendar/Calendar-Main.js
@@ -9,7 +9,7 @@ class CalendarMain extends Component {
 
     this.state = {
       currentMonth: new Date(),
-      selectedDate: new Date
+      selectedDate: new Date()
     }
 
     //bindings
@@ -49,15 +49,69 @@ class CalendarMain extends Component {
   }
 
   renderDays() {
-
+    const dateFormat = "dddd";
+    const days = [];
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+    for (let i = 1; i < 8; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      )
+    }
+    return <div className="days row">{days}</div>;
   }
 
   renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
 
+    const dateFormat = "D";
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? "disabled"
+                : dateFns.isSameDay(day, selectedDate)
+                  ? "selected"
+                  : ""
+            }`}
+            key={day}
+            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
   }
 
   onDateClick = (day) => {
-
+    this.setState({
+      selectedDate: day
+    })
   }
 
   nextMonth = () => {
