3a3333bc36d53ae5314f724340011acb1d05b300
mtamahori <mtamahori@gmail.com>
2019-03-04 11:48:18 -0600

    footer position fixed, messing w colors

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navbar, Footer } from './components'
 import Routes from './Routes'
+require('../public/stylesheets/app.css')
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Routes />
       <Footer />
--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,8 +4,13 @@ require('../../public/stylesheets/footer.css')
 
 const Footer = () => (
   <div className="footer">
-      <h4>FOOTER FOOTER FOOTER</h4>
-      <Icon name="music" size="small" />
+      <h4>DJDB - by Max Tamahori</h4>
+      <Icon name="inverted music icon" size="large" />
+      <Icon name="inverted music icon" size="large" />
+      <Icon name="inverted music icon" size="large" />
+      <Icon name="inverted music icon" size="large" />
+      <Icon name="inverted music icon" size="large" />
+      <Icon name="inverted music icon" size="large" />
   </div>
 )
 
