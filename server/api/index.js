57ab7dbbe5759886d2159dfd0230cb3b69e8c721
mtamahori <mtamahori@gmail.com>
2019-03-27 22:36:09 -0500

    messages routes

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ router.use('/users', require('./users'))
 router.use('/bookers', require('./bookers'))
 router.use('/deejays', require('./deejays'))
 router.use('/gigs', require('./gigs'))
+router.use('/messages', require('./messages'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
