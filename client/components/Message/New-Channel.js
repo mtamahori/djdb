2f16e61067f2a4b4b6edd22d1d56f75e86167243
mtamahori <mtamahori@gmail.com>
2019-04-04 15:31:13 -0500

    components

--- client/components/Message/New-Channel.js
+++ client/components/Message/New-Channel.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const NewChannel = (props) => {
-  return (
-    <h1>NewChannel</h1>
-  )
+class NewChannel extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>NewChannel</h1>
+    )
+  }
 }
 
-export default NewChannel
+const mapState = null;
+const mapDispatch = null;
+
+export default connect(mapState, mapDispatch)(NewChannel)
--- client/store/index.js
+++ client/store/index.js
@@ -6,9 +6,10 @@ import user from './user'
 import bookers from './bookers'
 import deejays from './deejays'
 import gigs from './gigs'
+import channels from './channels'
 import messages from './messages'
 
-const reducer = combineReducers({ user, bookers, deejays, gigs, messages })
+const reducer = combineReducers({ user, bookers, deejays, gigs, channels, messages })
 
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
@@ -20,4 +21,5 @@ export * from './user'
 export * from './bookers'
 export * from './deejays'
 export * from './gigs'
+export * from './channels'
 export * from './messages'
